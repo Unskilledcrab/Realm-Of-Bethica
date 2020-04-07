9c725193cffd91785e3f48786cbf42a8542975e1
Jeremy Buentello <jb09056@georgiasouthern.edu>
2020-04-06 21:07:41 -0400

    updating discord project to have a test channel

--- ROB.Discord/Models/Secrets/DiscordSecrets.cs
+++ ROB.Discord/Models/Secrets/DiscordSecrets.cs
@@ -9,6 +9,7 @@ namespace ROB.Discord.Models.Secrets
         public static ulong AnnouncementChannelId { get; set; }
         public static ulong BethicaChatId { get; set; }
         public static ulong CommunityGeneralChatId { get; set; }
+        public static ulong TestChatId { get; set; }
         public static ulong StaffId { get; set; }
         public static ulong GuildId { get; set; }
     }
