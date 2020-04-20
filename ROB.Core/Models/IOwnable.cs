4c28ce0be4313ff3e61748aed8635f8ec499f165
Jeremy Buentello <jb09056@georgiasouthern.edu>
2020-04-19 21:05:06 -0400

    needing to switch to publish

--- ROB.Core/Models/IOwnable.cs
+++ ROB.Core/Models/IOwnable.cs
@@ -18,12 +18,4 @@ namespace ROB.Core.Models
         ICollection<TLink> CharacterSheets { get; set; }
     }
 
-    public abstract class OwnableBase : IOwnable
-    {
-        public bool IsPublic { get; set; }
-        public string CreatorId { get; set; }
-        public ApplicationUser Creator { get; set; }
-        public DateTime CreationDateTime { get; set; }
-        public DateTime LastUpdate { get; set; }
-    }
 }
--- ROB.Core/Repositories/IChildSkillRepository.cs
+++ ROB.Core/Repositories/IChildSkillRepository.cs
@@ -3,5 +3,56 @@
 namespace ROB.Core.Repositories
 {
     public interface IChildSkillRepository : ILinkRepository<ChildSkillModel> { }
+    public interface IApplicationUserRepository : ILinkRepository<ApplicationUser> { }
+
+    public interface IArcanePowerAttributeRepository : ILinkRepository<ArcanePowerAttributeModel> { }
+
+    public interface IArcaneSphereRepository : ILinkRepository<ArcaneSphereModel> { }
+
+    public interface IArcaneSubgroupRepository : ILinkRepository<ArcaneSubgroupModel> { }
+
+    public interface IArmorRestorationRepository : IRepository<ArmorRestorationModel> { }
+
+
+    public interface IArmorRestrictionRepository : IRepository<ArmorRestrictionModel> { }
+
+    public interface IAttributeRepository : IRepository<AttributeModel> { }
+
+
+    public interface IBuildingRepository : ILinkRepository<BuildingModel> { }
+
+    public interface IBuildingRatingRepository : IRepository<BuildingRatingModel> { }
+
+    public interface IBuildingSpecialtyRepository : IRepository<BuildingSpecialtyModel> { }
+
+    public interface IBuildingTypeRepository : IRepository<BuildingTypeModel> { }
+
+    public interface ICharacterAttributeRepository : IRepository<CharacterAttributeModel> { }
+
+    public interface ICharacterSheetRepository : ILinkRepository<CharacterSheetModel> { }
+
+    public interface IDamageTypeRepository : IRepository<DamageTypeModel> { }
+
+    public interface IItemCategoryRepository : IRepository<ItemCategoryModel> { }
+
+    public interface IItemPackRepository : ILinkRepository<ItemPackModel> { }
+
+    public interface ILanguageGroupTypeRepository : IRepository<LanguageGroupTypeModel> { }
+
+    public interface ILanguageRepository : IRepository<LanguageModel> { }
+
+    public interface IModifierRepository : ILinkRepository<ModifierModel> { }
+
+    public interface IParentSkillRepository : ILinkRepository<ParentSkillModel> { }
+
+    public interface IPoisonClassRepository : IRepository<PoisonClassModel> { }
+
+    public interface IPoisonRepository : ILinkRepository<PoisonModel> { }
+
+    public interface IPoisonTypeRepository : IRepository<PoisonTypeModel> { }
+
+    public interface IPregenProfessionArchetypeRepository : ILinkRepository<PregenProfessionArchetypeModel> { }
+
+
 
 }
